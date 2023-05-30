447e64f71644ff54c20f2b460617de917d978f53
TTranchese <tommaso.tranchese.96@gmail.com>
2023-05-30 12:31:28 +0200

    initial commit

---
+++ src/main/java/co/develhope/corsExample/controllers/Controller.java
@@ -0,0 +1,10 @@
+package co.develhope.corsExample.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+}
